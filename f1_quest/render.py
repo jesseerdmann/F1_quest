14ea6d6c77b1d1348e39d9c1cd5d3f637b59ad7d
Jesse Erdmann <jesse@jesseerdmann.com>
2021-04-11 19:09:00 -0500

    Initial site to push to gh pages

--- f1_quest/render.py
+++ f1_quest/render.py
@@ -2,7 +2,7 @@ from f1_quest.answer_key import AnswerKey
 from jinja2 import Environment, FileSystemLoader
 
 
-def render_static_pages():
+def render_static_pages(base_url="https://jesseerdmann.github.io/F1_quest"):
     ak = AnswerKey()
     file_loader = FileSystemLoader('f1_quest/templates')
     env = Environment(loader=file_loader)
@@ -10,4 +10,5 @@ def render_static_pages():
     index = env.get_template('index.html')
 
     with open('site/index.html', 'w') as out:
-        out.write(index.render(overall_table=ak.get_overall_standings()))
+        out.write(index.render(overall_table=ak.get_overall_standings(), 
+            base_url=base_url))
